c6f78cc578279e7efc1feb7df9110b997dd365d7
VincentiaC <125953110+VincentiaC@users.noreply.github.com>
2023-04-24 23:43:53 +0700

    orer history page
    Page blm lengkap hanya sekedar tampilan

---
+++ order_history.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Customized Building Material</title>
+<link rel='stylesheet' href='css/woocommerce-layout.css' type='text/css' media='all'/>
+<link rel='stylesheet' href='css/woocommerce-smallscreen.css' type='text/css' media='only screen and (max-width: 768px)'/>
+<link rel='stylesheet' href='css/woocommerce.css' type='text/css' media='all'/>
+<link rel='stylesheet' href='css/font-awesome.min.css' type='text/css' media='all'/>
+<link rel='stylesheet' href='style.css' type='text/css' media='all'/>
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Oswald:400,500,700%7CRoboto:400,500,700%7CHerr+Von+Muellerhoff:400,500,700%7CQuattrocento+Sans:400,500,700' type='text/css' media='all'/>
+<link rel='stylesheet' href='css/easy-responsive-shortcodes.css' type='text/css' media='all'/>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+</head>
+
+<body class="home page page-template page-template-template-portfolio page-template-template-portfolio-php">
+    <div id="page">
+        <div class="container">
+            <header id="masthead" class="site-header">
+		    <div class="site-branding">
+			    <h1 class="site-title"><a href="index.php" rel="home">E-Marketplace for Building Material</a></h1>
+			    <h2 class="site-description">Custom Design - Made by Craftmen</h2>
+		    </div>
+		    <nav id="site-navigation" class="main-navigation">
+		        <button class="menu-toggle" style="display: flexbox;">Menu</button>
+		        <a class="skip-link screen-reader-text" href="#content">Skip to content</a>
+		        <div class="menu-menu-1-container">
+			        <ul id="menu-menu-1" class="menu">
+				        <li><a href="index.php">Home</a></li>
+				        <li><a href="shop.php">Shop</a></li>
+				        <li><a href="#">Pages</a>
+				            <ul class="sub-menu">
+					            <li><a href="blog.php">Blog</a></li>
+					            <li><a href="about.php">About Us</a></li>
+					            <li><a href="contact.php">Contact Us</a></li>
+				            </ul>
+				        </li>
+				        <li><a href="#">My Account</a>
+				            <ul class="sub-menu">
+					            <li><a href="orderH.php">Order History</a></li>
+					            <li><a href="Settings.php">Account Settings</a></li>
+					            <li><a href="account.php">Log Out</a></li>
+				            </ul>
+				        </li>
+			        </ul>
+	            </div>
+		    </nav>
+		</header>
+        </div>
+    </div>
+
+    <div id="content" class="site-content">
+		<div id="primary" class="content-area column full">
+            <h2>Order History</div>
+            <br>
+            <br>
+            <table class="table table-bordered">
+                    <tr>
+                        <th>Order Number</th>
+                        <th>Order Name</th>
+                        <th>Payment Date</th>
+                        <th>Payment Total</th>
+                        <th>Company Name</th>
+                    </tr>
+            </table>
+        </div>
+    </div>
+</body>
